bce6ea5c8ecd00c618f811068c0bccf06a6903e0
Devendra33 <47396740+Devendra33@users.noreply.github.com>
2020-05-27 12:38:43 +0530

    Update db1.py

--- Database Connection/db1.py
+++ Database Connection/db1.py
@@ -3,11 +3,11 @@
 import mysql.connector
 
 # to use a perticular database use database = "dbname"
-mydb = mysql.connector.connect(host = "localhost", user  = "root", passwd = "Hondadio@1", database = "tsp")
+mydb = mysql.connector.connect(host = "localhost", user  = "", passwd = "", database = "")
 mycursor = mydb.cursor()
-mycursor.execute("select * from salesman")   # table name(salesman)
+mycursor.execute("select * from <tablename>")   # table name(salesman)
 
-res = mycursor.fetchall()
+res = mycursor.fetchall()   # fetches all the data from mycursor.
 for i in res:
     print(i)
 
